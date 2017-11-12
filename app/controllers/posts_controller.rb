50274a1c175292ddfd3d5314b44a6bc479e6fe2e
Marcin Kopański <marcin.kopanski@gmail.com>
2017-11-12 15:57:58 +0100

    Create, Update, Delete of posts, plus validations

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,24 +1,49 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all.order('created_at DESC')
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
+    @post = Post.new
   end
 
   def create
     @post = Post.new(post_params)
-    @post.save
 
-    redirect_to @post
+    if @post.save
+      redirect_to @post
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @post.update params[:post].permit(:title, :body)
+      redirect_to @post
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @post.destroy
+
+    redirect_to root_path
   end
 
   private
+
+     def set_post
+        @post = Post.find(params[:id])
+     end
 
     def post_params
       params.require(:post).permit(:title, :body)
